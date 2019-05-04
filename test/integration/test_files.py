9f0d5a7267746d333026122886892029586577c2
Holger Hass <h.hass@gmx.net>
2019-05-04 16:51:38 +0200

    arg parsing and packaging

---
+++ test/integration/test_files.py
@@ -0,0 +1,11 @@
+from flint.launch import flint_str
+from util import flint_test
+
+# def test_twoliner(two_liner_content, two_liner_expected_output, output_file):
+#     processed = flint_str(two_liner_content)
+#     output_file.write(processed)
+#     assert processed == two_liner_expected_output
+
+
+def test_two_liner():
+    flint_test("two_liner.py")
