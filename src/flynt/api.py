122a07e6cbea2c4edb0c1588e10bb68af539afe7
Ilya <ikamenshchikov@gmail.com>
2019-06-07 10:59:21 +0200

    print amount of code size reduction

--- src/flynt/api.py
+++ src/flynt/api.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import traceback
+from typing import Tuple
 
 import astor
 
@@ -9,10 +10,10 @@ from flynt.process import fstringify_code_by_line
 
 blacklist = {'.tox', 'venv', 'site-packages', '.eggs'}
 
-def fstringify_file(filename):
+def fstringify_file(filename) -> Tuple[bool, int, int]:
     """
     :param filename:
-    :return: if the file was edited
+    :return: tuple: (if the file was edited, length of original code, length of new code)
     """
 
     try:
@@ -23,15 +24,15 @@ def fstringify_file(filename):
     except Exception as e:
         print(f"Skipping file {filename} due to {e}")
         traceback.print_exc()
-        return False
+        return False, len(contents), len(contents)
     else:
         if new_code == contents:
-            return False
+            return False, len(contents), len(contents)
 
         with open(filename, "w", encoding="utf-8") as f:
             f.write(new_code)
 
-        return True
+        return True, len(contents), len(new_code)
 
 
 def fstringify_dir(in_dir):
@@ -41,14 +42,18 @@ def fstringify_dir(in_dir):
 
 def fstringify_files(files, verbose=False, quiet=False):
     change_count = 0
+    total_charcount_original = 0
+    total_charcount_new = 0
     start_time = time.time()
     for f in files:
         if any(b in f[0] for b in blacklist):
             continue
         file_path = os.path.join(f[0], f[1])
-        changed = fstringify_file(file_path)
+        changed, charcount_original, charcount_new = fstringify_file(file_path)
         if changed:
             change_count += 1
+        total_charcount_original += charcount_original
+        total_charcount_new += charcount_new
         status = "yes" if changed else "no"
 
         if verbose and not quiet:
@@ -59,6 +64,8 @@ def fstringify_files(files, verbose=False, quiet=False):
     if not quiet:
         file_s = "s" if change_count != 1 else ""
         print(f"\nfstringified {change_count} file{file_s} in {total_time}s")
+        charcount_percent_reduction = 100 *(total_charcount_original - total_charcount_new) / total_charcount_original
+        print(f"character count in python source files was reduced by {charcount_percent_reduction:.2f}%")
 
 
 def fstringify(file_or_path, verbose=False, quiet=False):
