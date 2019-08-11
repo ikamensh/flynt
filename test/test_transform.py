a31c851d2c360f8fee081cbf3794a45e0e128eab
neunkasulle <jan.zenkner@gmail.com>
2019-08-11 21:30:57 +0000

    port over tests from pyupgrade
    Copy over the tests and exchange the calling method with the flynt
    appropriate method
    resolves #11

--- test/test_transform.py
+++ test/test_transform.py
@@ -123,3 +123,57 @@ def test_digit_grouping_2():
     assert meta['changed']
     assert new == expected
 
+
+@pytest.mark.parametrize(
+    's',
+    (
+        # syntax error
+        '(',
+        # invalid format strings
+        "'{'.format(a)", "'}'.format(a)",
+        # weird syntax
+        '"{}" . format(x)',
+        # spans multiple lines
+        '"{}".format(\n    a,\n)',
+        # starargs
+        '"{} {}".format(*a)', '"{foo} {bar}".format(**b)"',
+        # likely makes the format longer
+        '"{0} {0}".format(arg)', '"{x} {x}".format(arg)',
+        '"{x.y} {x.z}".format(arg)',
+        # bytestrings don't participate in `.format()` or `f''`
+        # but are legal in python 2
+        'b"{} {}".format(a, b)',
+        # for now, too difficult to rewrite correctly
+        '"{:{}}".format(x, y)',
+        '"{a[b]}".format(a=a)',
+        '"{a.a[b]}".format(a=a)',
+        # not enough placeholders / placeholders missing
+        '"{}{}".format(a)', '"{a}{b}".format(a=a)',
+    ),
+)
+def test_fix_fstrings_noop(s):
+    new, meta = transform_chunk(s)
+    assert not meta['changed']
+    assert new == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('"{} {}".format(a, b)', 'f"{a} {b}"'),
+        ('"{1} {0}".format(a, b)', 'f"{b} {a}"'),
+        ('"{x.y}".format(x=z)', 'f"{z.y}"'),
+        ('"{.x} {.y}".format(a, b)', 'f"{a.x} {b.y}"'),
+        ('"{} {}".format(a.b, c.d)', 'f"{a.b} {c.d}"'),
+        ('"hello {}!".format(name)', 'f"hello {name}!"'),
+        ('"{}{{}}{}".format(escaped, y)', 'f"{escaped}{{}}{y}"'),
+        ('"{}{b}{}".format(a, c, b=b)', 'f"{a}{b}{c}"'),
+        # TODO: poor man's f-strings?
+        # '"{foo}".format(**locals())'
+    ),
+)
+def test_fix_fstrings(s, expected):
+    new, meta = transform_chunk(s)
+    assert meta['changed']
+    assert new == expected
+
