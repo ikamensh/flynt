426f25f4cfc2d50a5f617c23cfcc29acb3eb6502
ikkamens <ikkamens@amazon.com>
2020-02-21 22:51:43 +0100

    return exit code for `python -m flynt`, remove redundant `if main` in __init__.py

--- src/flynt/__init__.py
+++ src/flynt/__init__.py
@@ -2,7 +2,7 @@
 from old "%-formatted" and .format(...) strings into Python 3.6+'s f-strings.
 Learn more about f-strings at https://www.python.org/dev/peps/pep-0498/"""
 
-__version__ = "0.43.1"
+__version__ = "0.43.2"
 
 import argparse
 import sys
@@ -85,7 +85,3 @@ def main():
         fail_on_changes=args.fail_on_change,
         transform_concat=args.transform_concats,
     )
-
-
-if __name__ == "__main__":
-    sys.exit(main())
--- src/flynt/__main__.py
+++ src/flynt/__main__.py
@@ -1,6 +1,7 @@
-""" This file gets run when we call
-python3 -m flynt """
+""" This file gets run when we call `python3 -m flynt` """
+
+import sys
 
 from flynt import main
 
-main()
+sys.exit(main())
