e0147d0a1ce3d305360c072e890dc1ebd80370b6
Matías Lang <shareman1204@gmail.com>
2020-09-20 06:06:26 -0300

    Improve formatting of .format() with string literals inside it (#75)
    * Improve formatting of .format() with string literals inside it
    
    I didn't want something like f"Hello {'world'}" in my code. This change
    replaces trivial uses of f-strings (such as the one described before) by
    putting string literals directly inside the format string.
    
    * Fix formatting .format() with string literals for Python 3.7
    
    It was only working with Python 3.8. In Python 3.7 the rewritting wasn't
    working because ast.Str is used instead of ast.Constant.

--- src/flynt/transform/format_call_transforms.py
+++ src/flynt/transform/format_call_transforms.py
@@ -1,4 +1,5 @@
 import ast
+import sys
 import string
 from collections import deque
 
@@ -104,5 +105,30 @@ def joined_string(fmt_call: ast.Call) -> ast.JoinedStr:
         raise FlyntException(
             f"Some variables were never used: {var_map} - skipping conversion, it's a risk of bug."
         )
+
+    def is_literal_string(node):
+        if sys.version_info < (3, 8):
+            return isinstance(node, ast.Str)
+        else:
+            return (isinstance(node, ast.Constant) and
+                    isinstance(node.value, str))
+
+    def literal_string_value(node):
+        if sys.version_info < (3, 8):
+            return node.s
+        else:
+            return node.value
+
+    def fix_literals(segment):
+        if (isinstance(segment, ast.FormattedValue) and
+                segment.format_spec is None and
+                is_literal_string(segment.value)):
+            return segment.value
+        return segment
+    new_segments = [fix_literals(e) for e in new_segments]
+
+    if all(is_literal_string(segment)
+           for segment in new_segments):
+        return ast.Str(''.join(literal_string_value(segment) for segment in new_segments))
 
     return ast.JoinedStr(new_segments)
---
+++ test/integration/samples_in/literal_string.py
@@ -0,0 +1,7 @@
+n = 5
+x = "{} hello, {}".format(n, "world")
+x = "{} non-str {}".format(n, True)
+x = "{} with quote {}".format(n, '"x"')
+
+x = "hello, {}".format("world")
+x = "with quote {}".format('"x"')
