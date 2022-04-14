6d2a360bb2d489f9bf82d8e79853cc534cbc76c9
Ilya Kamen <ilya.kamenshchikov@bosch.com>
2022-04-14 17:44:48 +0200

    debug statement

--- test/integration/test_cli.py
+++ test/integration/test_cli.py
@@ -174,7 +174,7 @@ def test_cli_dry_run(monkeypatch, capsys, sample_file):
 
     # Check that the output includes all changed lines, and the farewell message
     out, err = capsys.readouterr()
-
+    print(out)
     for line in source_lines:
         if line not in converted_lines:
             assert f"-{line}" in out, "Original source line missing from output"
