e9ed2c3f3e9195b397cb2b639b3eecdeb4bbaa64
Ilya Kamen <ilya.kamenshchikov@bosch.com>
2023-06-17 15:35:40 +0200

    mypy 2

--- src/flynt/transform/format_call_transforms.py
+++ src/flynt/transform/format_call_transforms.py
@@ -89,7 +89,7 @@ def joined_string(
 
     if all(isinstance(segment, ast.Str) for segment in new_segments):
         return (
-            ast.Str("".join(segment.value for segment in new_segments)),
+            ast.Str("".join(segment.value for segment in new_segments)),  #type:ignore[misc]
             False,
         )
 
