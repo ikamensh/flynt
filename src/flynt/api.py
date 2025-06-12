ed320759d8a4275d84385aea8618d2ae1105fdd7
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2025-06-11 23:00:07 -0400

    fix linting

--- src/flynt/api.py
+++ src/flynt/api.py
@@ -5,7 +5,6 @@ import logging
 import os
 import sys
 import time
-import traceback
 from difflib import unified_diff
 from typing import Collection, List, Optional, Tuple
 
@@ -101,7 +100,9 @@ def fstringify_code(
                     state=state,
                 )
             except Exception as exc:
-                log.error("Transforming concatenation of literal strings failed", exc_info=exc)
+                log.error(
+                    "Transforming concatenation of literal strings failed", exc_info=exc
+                )
             else:
                 changes += concat_changes
                 state.concat_changes += concat_changes
@@ -112,7 +113,9 @@ def fstringify_code(
                     state=state,
                 )
             except Exception as exc:
-                log.error("Transforming concatenation of literal strings failed", exc_info=exc)
+                log.error(
+                    "Transforming concatenation of literal strings failed", exc_info=exc
+                )
             else:
                 changes += join_changes
                 state.join_changes += join_changes
