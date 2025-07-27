e3ce430e0c79a3332eb96287b29540f839d224f6
Ilya Kamen <ikamenshchikov@gmail.com>
2025-07-27 21:51:41 +0200

    test: ensure conversions still run alongside skipped concat

--- test/integration/expected_out_concat/regex_sub.py
+++ test/integration/expected_out_concat/regex_sub.py
@@ -3,7 +3,11 @@
 import re
 
 
+spam = f"foo bar"
+
 individual_tests = [
     re.sub(r"\.py$", "", test) + ".py" for test in ["a.py", "b.py", "c*"] if not test.endswith('*')
 ]
+
+spam2 = f"foo bar"
 
--- test/integration/samples_in_concat/regex_sub.py
+++ test/integration/samples_in_concat/regex_sub.py
@@ -3,7 +3,11 @@
 import re
 
 
+spam = "%s %s" % ("foo", "bar")
+
 individual_tests = [
     re.sub(r"\.py$", "", test) + ".py" for test in ["a.py", "b.py", "c*"] if not test.endswith('*')
 ]
+
+spam2 = "%s %s" % ("foo", "bar")
 
