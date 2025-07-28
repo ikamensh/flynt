b6179c10e744b641f5f180cfc40e95c90a8ad18c
ikamen <ikamenshchikov@gmail.com>
2025-07-28 14:57:10 +0200

    new version

--- src/flynt/__init__.py
+++ src/flynt/__init__.py
@@ -2,7 +2,7 @@
 from old "%-formatted" and .format(...) strings into Python 3.6+'s f-strings.
 Learn more about f-strings at https://www.python.org/dev/peps/pep-0498/"""
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 from flynt.cli import main
 
